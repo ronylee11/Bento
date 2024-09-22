c9dbc0a8e08093bc25e23799ed3f1111a6683620
Rony Lee <rongyil33@gmail.com>
2024-09-22 22:41:31 +0800

    Add bootstrap cdn, add search bar

--- config.js
+++ config.js
@@ -228,3 +228,20 @@ const CONFIG = {
     },
   ],
 };
+
+const form = document.querySelector("#searchbar");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+const search_google = document.querySelector("#search_google");
+search_google.addEventListener("click", () => {
+    const search = document.querySelector("#search").value;
+    window.location.href = `https://www.google.com/search?q=${search}`;
+});
+
+const search_youtube = document.querySelector("#search_youtube");
+search_youtube.addEventListener("click", () => {
+    const search = document.querySelector("#search").value;
+    window.location.href(`https://www.youtube.com/results?search_query=${search}`);
+});
