eda6a5259d3cf3d8a7e0f81d76555fa439cb4a58
Rony Lee <rongyil33@gmail.com>
2024-09-23 12:26:15 +0800

    search bar empty will do nothing

--- config.js
+++ config.js
@@ -237,11 +237,13 @@ form.addEventListener("submit", (e) => {
 const search_google = document.querySelector("#search_google");
 search_google.addEventListener("click", () => {
     const search = document.querySelector("#search").value;
+    if (search === "") return;
     window.location.href = `https://www.google.com/search?q=${search}`;
 });
 
 const search_youtube = document.querySelector("#search_youtube");
 search_youtube.addEventListener("click", () => {
     const search = document.querySelector("#search").value;
+    if (search === "") return;
     window.location.href = `https://www.youtube.com/results?search_query=${search}`;
 });
